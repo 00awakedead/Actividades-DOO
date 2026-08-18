a12b4afcdd20e6353604d816a92aec4febad1d9c
00awakedead <abelcabero019@gmail.com>
2026-08-17 22:05:42 -0400

    Desafio Clase 3 realizado

--- Desafio Clase 3/Reserva.java
+++ Desafio Clase 3/Reserva.java
@@ -1,31 +1,32 @@
 public class Reserva {
-    private String nombre;
+    private String producto;
     private double precio;
     private int cantidadDisponible;
 
-    public Reserva(String nombre, double precio, int cantidadDisponible){
-        this.nombre = nombre;
+    public Reserva(String producto, double precio, int cantidadDisponible){
+        this.producto = producto;
         this.precio = precio;
         this.cantidadDisponible = cantidadDisponible;
     }
 
     public void mostrarInformacion(){
         System.out.println("----- PRODUCTO -----");
-        System.out.println("Nombre: " + nombre);
+        System.out.println("Nombre: " + producto);
         System.out.println("Precio:$ " + precio);
         System.out.println("Cantidad: " + cantidadDisponible);
     }
 
     public void reservar(int cantidad){
 
-        if (cantidad <= cantidadDisponible) {
-            cantidadDisponible -= cantidad;
-
-            System.out.println("Venta realizada: " + cantidad +  " unidad(es)");
-
-        }else {
+        if (cantidad <= 0){
+            System.out.println("La cantidad a reservar debe ser mayor a 0");
+        }else if (cantidad > cantidadDisponible) {
             System.out.println("El stock es insuficiente");
-
+        }
+        else{
+            cantidadDisponible -= cantidad;
+            System.out.println("Venta realizada: " + cantidad + " unidad(es)");
+            System.out.println("Stock restante: " +  cantidadDisponible);
         }
     }
 
@@ -34,13 +35,21 @@ public class Reserva {
         return precioConIva;
     }
 
-    public double calcularTotal(int cantidad, double descuento){
-        double total = precio * cantidad;
-        double montoConDescuento = total * descuento/100;
+    public double calcularTotal(int cantidad) {
+        if (cantidad <= 0 || cantidad > cantidadDisponible) {
+            System.out.println("Cantidad invalida.");
+            return 0;
+        }
+        return precio * cantidad;
+    }
+
+    public double calcularTotal(int cantidad, double descuento) {
+        double total = calcularTotal(cantidad);
+
+        double montoConDescuento = total * descuento / 100;
         double totalConDescuento = total - montoConDescuento;
 
         return totalConDescuento;
-
     }
 
 }
